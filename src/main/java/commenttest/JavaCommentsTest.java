e76ccfbc7f338a542cab42d078949c0246efba43
mkuznyetsov <userdfdc@some.mail>
2017-03-24 10:44:37 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1490304716793
+//first_change1490350377970
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
